f7628cb0390766b2615c1248d291c33f0b464d26
laike9m <laike9m@gmail.com>
2014-05-10 16:51:21 +0800

    automatically write to README done

--- list_songs.py
+++ list_songs.py
@@ -3,11 +3,14 @@ from __future__ import print_function
 import glob
 import os
 import unicodedata
-import eyed3
-
 import sys
+import eyed3
+from collections import namedtuple
+
 fse = sys.getfilesystemencoding()
 files = [x for x in glob.glob(u"songs/*.mp3")]
+SongInfo = namedtuple("SongInfo", "title_artist album")
+songinfo_list = []
 
 
 def calc_string_width(s):
@@ -19,12 +22,12 @@ def calc_string_width(s):
             extra_width += 1
     return extra_width, len(s) + extra_width
 
-with open("songs.txt", 'wt') as output, open("gen_table.txt", 'wt') as gt:
+with open("songs.txt", 'wt') as output:
     for f in files:
         song = eyed3.load(f)
-        title = os.path.split(f)[1]
-        album = song.tag.album
+        title = os.path.split(f)[1][:-4]  # strip .mp3
         artist = song.tag.artist
+        album = song.tag.album
         try:
             # 有些信息是按latin1 decode的, 变成\xcb\xea这样, 但实际上应该是gbk decode, 即两个字节应该一起读
             # 所以要先encode回bytes再用gbk decode
@@ -33,6 +36,7 @@ with open("songs.txt", 'wt') as output, open("gen_table.txt", 'wt') as gt:
             album = album.encode('latin1').decode('gbk')
         except UnicodeEncodeError:
             pass
+        songinfo_list.append(SongInfo(title+' - '+artist, album))
         extra_t_width, t_width = calc_string_width(title)
         extra_ar_width, ar_width = calc_string_width(artist)
         extra_al_width, al_width = calc_string_width(album)
@@ -46,22 +50,19 @@ with open("songs.txt", 'wt') as output, open("gen_table.txt", 'wt') as gt:
             format_string = u'{:%d}' % (70-extra_t_width-extra_ar_width)
             output.write(format_string.format(title + ' - ' + artist).encode('utf-8'))
             output.write(album.encode('utf-8') + '  \n')
-        gt.write((title + ' - ' + artist + ';' + album + '\n').encode('utf-8'))
-        # paste gen_table.txt to http://www.tablesgenerator.com/markdown_tables#
-        # semicolon is seperator
-        # then copy/paste generated markdown table to README.md
 
 
 def rewrite_readme():
     import codecs
-    with codecs.open('songs.txt', 'r', 'utf-8') as songs,\
-            codecs.open('README.md', 'w') as readme:
+    with codecs.open('README.md', 'w') as readme:
         readme_title_text = u"laike9m ACG 音樂精選  \n=======\n\n個人向ACG音樂精選\n\n"
         readme.write(readme_title_text.encode('utf-8'))
-        for line in songs.readlines():
-            readme.write('\t' + line.rstrip().encode('utf-8') + '\n')
+        # draw markdown table
+        head = "| title - artist | album |\n"
+        seperator = "|---|---|\n"
+        readme.write(head + seperator)
+        for songinfo in songinfo_list:
+            readme.write('|' + songinfo.title_artist.encode('utf-8') + '|' +
+                         songinfo.album.encode('utf-8') + '\n')
 
-# rewrite_readme()
-
-
-# TODO: github页面字体宽度问题
+rewrite_readme()
